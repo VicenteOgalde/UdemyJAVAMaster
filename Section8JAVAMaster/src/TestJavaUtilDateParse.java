31d756c492be12b7d30c02e907fa659b92b1a57f
vicente_ogalde <vicenteogalde@gmail.com>
2022-12-23 11:51:42 -0300

    section8 commit 1

--- Section8JAVAMaster/src/TestJavaUtilDateParse.java
+++ Section8JAVAMaster/src/TestJavaUtilDateParse.java
@@ -1,15 +1,25 @@
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Scanner;
 
 public class TestJavaUtilDateParse {
     public static void main(String[] args) {
 
+        Scanner sc= new Scanner(System.in);
         SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        System.out.println("type your date with this format 'yyyy-MM-dd' : ");
+
         try {
-            Date date= format.parse("2000-03-22");
+            Date date= format.parse(sc.next());
             System.out.println(date);
             System.out.println(format.format(date));
+
+            Date date2= new Date();
+
+            String result=date.after(date2)?"you are from the future":"you are from the past";
+
+            System.out.println(result);
         } catch (ParseException e) {
             throw new RuntimeException(e);
         }
