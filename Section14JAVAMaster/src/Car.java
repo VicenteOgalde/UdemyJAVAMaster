76be6e9ec32a2ebb5a334b45888f7379c91b8c1f
vicente_ogalde <vicenteogalde@gmail.com>
2022-12-28 10:03:16 -0300

    section 14 commit 1

--- Section14JAVAMaster/src/Car.java
+++ Section14JAVAMaster/src/Car.java
@@ -1,9 +1,58 @@
 public class Car {
 
-    String company;
-    String model;
-    String color;
-    double cc;
 
+    private String company;
+    private String model;
+    private String color;
+    private double cc;
+    private int gasCapacity;
 
+    public Car(String company, String model) {
+        this.company = company;
+        this.model = model;
+    }
+
+    public Car() {
+
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double getCc() {
+        return cc;
+    }
+
+    public void setCc(double cc) {
+        this.cc = cc;
+    }
+
+    public int getGasCapacity() {
+        return gasCapacity;
+    }
+
+    public void setGasCapacity(int gasCapacity) {
+        this.gasCapacity = gasCapacity;
+    }
 }
--- Section14JAVAMaster/src/CarImpl.java
+++ Section14JAVAMaster/src/CarImpl.java
@@ -1,6 +1,11 @@
 public class CarImpl {
     public static void main(String[] args) {
 
+    Car car= new Car();
+    Car car2 = new Car("mazda","gt1");
+
+        System.out.println("car.color = " + car.getColor());
+
 
 
 
