14a5568a31ce5e618e2cc6a531436530ed5eddc3
vicente_ogalde <vicenteogalde@gmail.com>
2022-12-30 09:09:07 -0300

    section 18 commit 2

---
+++ Section18JAVAMaster/src/cl/vicoga/inheritance/InternationalStudent.java
@@ -0,0 +1,14 @@
+package cl.vicoga.inheritance;
+
+public class InternationalStudent extends Student{
+
+    private String country;
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
--- Section18JAVAMaster/src/cl/vicoga/inheritance/TestInheritance.java
+++ Section18JAVAMaster/src/cl/vicoga/inheritance/TestInheritance.java
@@ -3,12 +3,28 @@ package cl.vicoga.inheritance;
 public class TestInheritance {
     public static void main(String[] args) {
 
-        Person student = new Student();
+        Student student = new Student();
         student.setName("st1");
+        student.setSchool("U 26");
+
+        InternationalStudent studentInt= new InternationalStudent();
+        studentInt.setName("pete");
+        studentInt.setCountry("UK");
+
 
         Teacher teacher = new Teacher();
         teacher.setName("tc1");
-        
+
+        Class stInt= studentInt.getClass();
+        while (stInt.getSuperclass()!=null){
+
+            System.out.println("child class: "+stInt.getClass().getName()+
+                    " class father: "+stInt.getSuperclass().getName());
+
+            stInt=stInt.getSuperclass();
+        }
+
+
 
 
     }
