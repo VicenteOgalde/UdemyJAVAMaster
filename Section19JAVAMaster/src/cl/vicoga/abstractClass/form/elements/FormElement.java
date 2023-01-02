282e271acddd52d72c95bed2d3ffc71b982cc41a
vicente_ogalde <vicenteogalde@gmail.com>
2023-01-02 14:33:03 -0300

    section 19 commit 10

--- Section19JAVAMaster/src/cl/vicoga/abstractClass/form/elements/FormElement.java
+++ Section19JAVAMaster/src/cl/vicoga/abstractClass/form/elements/FormElement.java
@@ -1,11 +1,20 @@
 package cl.vicoga.abstractClass.form.elements;
 
+import cl.vicoga.abstractClass.form.validator.Validator;
+
+import java.util.ArrayList;
+import java.util.List;
+
 abstract public class FormElement {
 
     protected String name;
     protected String value;
 
+    private List<Validator> validators;
+    private List<String> errors;
     public FormElement() {
+        this.validators= new ArrayList<>();
+        this.errors= new ArrayList<>();
     }
 
     public FormElement(String name) {
@@ -16,6 +25,22 @@ abstract public class FormElement {
     public void setValue(String value) {
         this.value = value;
     }
+    public FormElement addValidate(Validator validator){
+        this.validators.add(validator);
+        return this;
+    }
 
+    public List<String> getErrors() {
+        return errors;
+    }
+
+    public boolean isValidated(){
+        this.validators.forEach(v->{
+            if(!v.isValidated(this.value)){
+                this.errors.add(v.getMessage());
+            }
+        });
+        return this.errors.isEmpty();
+    }
     public abstract String drawHtml();
 }
