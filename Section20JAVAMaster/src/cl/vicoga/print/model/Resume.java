1ab209b4765f8f226794c33978e83fe2b635ffe8
vicente_ogalde <vicenteogalde@gmail.com>
2023-01-03 21:15:44 -0300

    section 20 commit 1

---
+++ Section20JAVAMaster/src/cl/vicoga/print/model/Resume.java
@@ -0,0 +1,42 @@
+package cl.vicoga.print.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Resume extends Sheet{
+
+    private String name;
+    private String career;
+    private List<String> exp;
+
+    public Resume(String content) {
+        super(content);
+        this.exp= new ArrayList<String>();
+    }
+
+    public Resume(String content, String name, String career) {
+        this(content);
+        this.name = name;
+        this.career = career;
+
+    }
+    public Resume addExp(String ex){this.exp.add(ex); return this;}
+
+    @Override
+    public String print() {
+        StringBuilder sb = new StringBuilder("name: ");
+        sb.append(this.name).append("\n")
+                .append("career: ").append(this.career)
+                .append("\n")
+                .append(this.content).append("\n")
+                .append("Experiences: ");
+
+        this.exp.forEach(e->{
+            sb.append("\n")
+                    .append("-")
+                    .append(e);
+        });
+
+        return sb.toString();
+    }
+}
