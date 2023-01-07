16223225ee13ae51f8b5f2b1a00c33733cf843e9
vicente_ogalde <vicenteogalde@gmail.com>
2023-01-07 18:35:01 -0300

    section 24 commit n8

---
+++ Section24JAVAMaster/src/cl/vicoga/set/TestLinkedList.java
@@ -0,0 +1,48 @@
+package cl.vicoga.set;
+
+import cl.vicoga.set.model.Student;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class TestLinkedList {
+    public static void main(String[] args) {
+
+
+        LinkedList<Student> t= new LinkedList<>();
+        System.out.println(t+ ", size "+t.size());
+        System.out.println(t+" is empty "+t.isEmpty());
+
+        t.add(new Student("tex",5));
+        t.add(new Student("tex",5));
+        t.add(1,new Student("tix",4));
+        t.add(new Student("tox",7));
+        t.set(0,new Student("tax",3));
+
+        System.out.println(t);
+
+        t.addFirst(new Student("tt1",8));
+
+        t.addLast(new Student("zz1",10));
+
+        System.out.println(t);
+
+        System.out.println("t.getFirst() = " + t.getFirst());
+
+        System.out.println("t.getLast() = " + t.getLast());
+
+        
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
