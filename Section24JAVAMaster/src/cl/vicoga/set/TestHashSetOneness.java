b4f36431b1a23c89fc8e9200d70d31e1714e0242
vicente_ogalde <vicenteogalde@gmail.com>
2023-01-07 16:28:56 -0300

    section 24 commit n 5

--- Section24JAVAMaster/src/cl/vicoga/set/TestHashSetOneness.java
+++ Section24JAVAMaster/src/cl/vicoga/set/TestHashSetOneness.java
@@ -2,16 +2,17 @@ package cl.vicoga.set;
 
 import cl.vicoga.set.model.Student;
 
+import java.util.HashSet;
 import java.util.Set;
 import java.util.TreeSet;
 
-public class TestTreeSetStudent {
+public class TestHashSetOneness {
     public static void main(String[] args) {
 
 
-        Set<Student> t= new TreeSet<>((a,b)->a.getGrade().compareTo(b.getGrade()));
+        Set<Student> t= new HashSet<>();
         t.add(new Student("tex",5));
-        t.add(new Student("texx",5));
+        t.add(new Student("tex",5));
         t.add(new Student("tix",4));
         t.add(new Student("tox",7));
         t.add(new Student("tax",3));
--- Section24JAVAMaster/src/cl/vicoga/set/model/Student.java
+++ Section24JAVAMaster/src/cl/vicoga/set/model/Student.java
@@ -1,5 +1,7 @@
 package cl.vicoga.set.model;
 
+import java.util.Objects;
+
 public class Student implements Comparable<Student>{
     private String name;
     private Integer grade;
@@ -42,5 +44,18 @@ public class Student implements Comparable<Student>{
             return 0;
         }
         return this.name.compareTo(o.getName());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return Objects.equals(name, student.name) && Objects.equals(grade, student.grade);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, grade);
     }
 }
