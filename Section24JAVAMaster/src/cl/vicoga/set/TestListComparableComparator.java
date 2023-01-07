34d7b9f69f45e6c553d11a30dbd87c585b97c696
vicente_ogalde <vicenteogalde@gmail.com>
2023-01-07 17:35:36 -0300

    section 24 commit n7

---
+++ Section24JAVAMaster/src/cl/vicoga/set/TestListComparableComparator.java
@@ -0,0 +1,31 @@
+package cl.vicoga.set;
+
+import cl.vicoga.set.model.Student;
+
+import java.util.*;
+
+public class TestListComparableComparator {
+    public static void main(String[] args) {
+
+
+        List<Student> t= new ArrayList<>();
+        t.add(new Student("tex",5));
+        t.add(new Student("tex",5));
+        t.add(new Student("tix",4));
+        t.add(new Student("tox",7));
+        t.add(new Student("tax",3));
+        t.add(new Student("tax",3));
+
+        System.out.println(t);
+
+        //t.sort(Student::compareTo);
+        t.sort(Comparator.comparing(Student::getGrade));
+        //Collections.sort(t,(a,b)->a.getGrade().compareTo(b.getGrade()));
+        t.forEach(System.out::println);
+
+
+
+
+
+    }
+}
