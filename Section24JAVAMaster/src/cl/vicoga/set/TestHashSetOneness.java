462a4b78e9b0b4ab6574120d74a4f62ba6548e81
vicente_ogalde <vicenteogalde@gmail.com>
2023-01-07 16:37:50 -0300

    section 24 commit n6

--- Section24JAVAMaster/src/cl/vicoga/set/TestHashSetOneness.java
+++ Section24JAVAMaster/src/cl/vicoga/set/TestHashSetOneness.java
@@ -3,6 +3,7 @@ package cl.vicoga.set;
 import cl.vicoga.set.model.Student;
 
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -18,6 +19,17 @@ public class TestHashSetOneness {
         t.add(new Student("tax",3));
 
         System.out.println(t);
+
+        t.forEach(System.out::println);
+
+        Iterator<Student> it = t.iterator();
+
+        while (it.hasNext()){
+            Student st = it.next();
+            System.out.println(st.getName());
+        }
+
+
 
     }
 }
