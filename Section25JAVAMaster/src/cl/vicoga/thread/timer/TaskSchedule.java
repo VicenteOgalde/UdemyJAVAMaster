826593081795efd9b2572c9ec54229bbb5684ab6
vicente_ogalde <vicenteogalde@gmail.com>
2023-01-08 14:38:56 -0300

    section 25 commit n9

--- Section25JAVAMaster/src/cl/vicoga/thread/timer/TaskSchedule.java
+++ Section25JAVAMaster/src/cl/vicoga/thread/timer/TaskSchedule.java
@@ -3,21 +3,29 @@ package cl.vicoga.thread.timer;
 import java.util.Date;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class TaskSchedule {
     public static void main(String[] args) {
 
+        AtomicInteger ai= new AtomicInteger(3);
+
         Timer timer = new Timer();
         timer.schedule(new TimerTask() {
+
             @Override
             public void run() {
-                System.out.println("Task date "
-                +new Date()+" name of the thread "+
-                        Thread.currentThread().getName());
-                System.out.println("Finish");
-                timer.cancel();
+                int count=ai.getAndDecrement();
+                if (count>0) {
+                    System.out.println("Task date "
+                            + new Date() + " name of the thread " +
+                            Thread.currentThread().getName());
+                }else {
+                    System.out.println("Finish");
+                    timer.cancel();
+                }
             }
-        },5000);
+        },1000,1000);
 
         System.out.println("task after 5 sec : "+new Date());
 
