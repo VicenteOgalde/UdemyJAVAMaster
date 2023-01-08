52df0c2dbc6c36a5c9384601fe8d68e4078e7201
vicente_ogalde <vicenteogalde@gmail.com>
2023-01-08 13:37:01 -0300

    section 25 commit n5

---
+++ Section25JAVAMaster/src/cl/vicoga/thread/testsync/BreadFactory.java
@@ -0,0 +1,33 @@
+package cl.vicoga.thread.testsync;
+
+public class BreadFactory {
+
+    private String bread;
+    private boolean available;
+
+    public synchronized void bake(String pm){
+        while (available){
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+        this.bread=pm;
+        System.out.println("baker make "+this.bread);
+        this.available=true;
+        notify();
+
+    }
+    public synchronized  String consume() throws InterruptedException {
+        while (!available){
+            wait();
+        }
+        System.out.println("client consume "+this.bread);
+        this.available=false;
+        notify();
+        return this.bread;
+    }
+}
