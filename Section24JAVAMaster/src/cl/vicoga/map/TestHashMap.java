63be2d5717ce1da135815bdcb05bf16e20f8e61b
vicente_ogalde <vicenteogalde@gmail.com>
2023-01-07 21:34:21 -0300

    section 24 commit n11

--- Section24JAVAMaster/src/cl/vicoga/map/TestHashMap.java
+++ Section24JAVAMaster/src/cl/vicoga/map/TestHashMap.java
@@ -1,7 +1,9 @@
 package cl.vicoga.map;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class TestHashMap {
     public static void main(String[] args) {
@@ -17,6 +19,29 @@ public class TestHashMap {
         String name = person.get("name");
 
         System.out.println("name = " + name);
+
+        person.remove("nick");
+
+        System.out.println("person = " + person);
+
+        boolean b=person.containsKey("age");
+        System.out.println("exist age key "+b);
+
+        b=person.containsValue("55");
+        System.out.println("exist 55 value "+b);
+
+        Collection<String> values= person.values();
+
+        System.out.println("values = " + values);
+
+        Set<String> keys= person.keySet();
+
+        keys.forEach(System.out::println);
+
+        person.forEach((k,v)-> System.out.println("key: "+k+", value: "+v));
+
+
+
 
 
 
