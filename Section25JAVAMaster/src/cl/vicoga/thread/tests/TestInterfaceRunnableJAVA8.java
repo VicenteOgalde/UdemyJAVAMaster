43cc9a42383192639feac64504180f26a333b05c
vicente_ogalde <vicenteogalde@gmail.com>
2023-01-08 11:04:35 -0300

    section 25 commit n3

--- Section25JAVAMaster/src/cl/vicoga/thread/tests/TestInterfaceRunnableJAVA8.java
+++ Section25JAVAMaster/src/cl/vicoga/thread/tests/TestInterfaceRunnableJAVA8.java
@@ -3,19 +3,24 @@ package cl.vicoga.thread.tests;
 import cl.vicoga.thread.tests.runnable.TravelTask;
 
 public class TestInterfaceRunnableJAVA8 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+
+        Thread main= Thread.currentThread();
+
 
         Runnable travel = new Runnable() {
             @Override
             public void run() {
                 for (int i=0;i<10;i++){
                     System.out.println(i+" - "+Thread.currentThread());
+
                     try {
                         Thread.sleep(10);
                     } catch (InterruptedException e) {
                         throw new RuntimeException(e);
                     }
                 }
+                System.out.println("state main thread: "+main.getState());
                 System.out.println("finish execution");
             }
         };
@@ -29,13 +34,24 @@ public class TestInterfaceRunnableJAVA8 {
                         throw new RuntimeException(e);
                     }
                 }
+            System.out.println("state main thread: "+main.getState());
                 System.out.println("finish execution");
            };
 
 
-        new Thread(travel,"paris").start();
-        new Thread(travel,"Usa").start();
-        new Thread(travel2,"UK").start();
+        Thread t1= new Thread(travel,"paris");
+        Thread t2= new Thread(travel,"Usa");
+        Thread t3= new Thread(travel2,"UK");
+
+        t1.start();
+        t2.start();
+        t3.start();
+
+        t1.join();
+        t2.join();
+        t3.join();
+
+        System.out.println("continue main method.....");
 
 
 
