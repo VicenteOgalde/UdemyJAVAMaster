f772a324fe4abd52e121c82af35dca226be30455
vicente_ogalde <vicenteogalde@gmail.com>
2023-01-09 09:45:23 -0300

    section 27 commit n0

---
+++ Section27JAVAMaster/src/cl/vicoga/api/stream/tests/TestStream.java
@@ -0,0 +1,35 @@
+package cl.vicoga.api.stream.tests;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class TestStream {
+
+    public static void main(String[] args) {
+
+/*
+        Stream<String> names=Stream.of("tex","tax","tix","tux");
+
+        names.forEach(System.out::println);
+
+
+        Stream<String> name= Stream.<String>builder().add("asd").add("qwe").add("123").add("zxc").build();
+
+        name.forEach(e-> System.out.println("e = " + e));
+*/
+        List<Integer> numbers= new ArrayList<>();
+        numbers.add(1);
+        numbers.add(5);
+        numbers.add(6);
+        numbers.add(18);
+
+        Stream<Integer> n=numbers.stream();
+
+        
+
+
+
+    }
+
+}
