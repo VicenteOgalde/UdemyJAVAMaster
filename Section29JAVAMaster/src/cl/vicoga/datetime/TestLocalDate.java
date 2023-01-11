15d899dc778bc4f064cfab115c92de6608a196f7
vicente_ogalde <vicenteogalde@gmail.com>
2023-01-11 10:52:06 -0300

    section 29 commit n1

--- Section29JAVAMaster/src/cl/vicoga/datetime/TestLocalDate.java
+++ Section29JAVAMaster/src/cl/vicoga/datetime/TestLocalDate.java
@@ -1,5 +1,6 @@
 package cl.vicoga.datetime;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 
 public class TestLocalDate {
@@ -13,6 +14,21 @@ public class TestLocalDate {
 
         System.out.println("actualDate = " + actualDate);
 
+        actualDate = LocalDate.parse("2020-02-01");
 
+        System.out.println("actualDate = " + actualDate);
+
+        LocalDate tDay = LocalDate.now().plusDays(1);
+
+        System.out.println("tDay = " + tDay);
+        
+        LocalDate lastM= LocalDate.now().minusMonths(1);
+
+        System.out.println("lastM = " + lastM);
+
+        DayOfWeek dw = LocalDate.parse("2021-08-06").getDayOfWeek();
+
+        System.out.println("dw = " + dw);
+        
     }
 }
