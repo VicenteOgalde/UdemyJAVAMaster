c76a19ba698a05c500b0a99316f4801645058267
vicente_ogalde <vicenteogalde@gmail.com>
2023-01-11 21:33:17 -0300

    section 30 commit n1

--- Section30JAVAMaster/src/cl/vicoga/annotations/TestAnnotation.java
+++ Section30JAVAMaster/src/cl/vicoga/annotations/TestAnnotation.java
@@ -2,7 +2,9 @@ package cl.vicoga.annotations;
 
 import cl.vicoga.annotations.model.Product;
 
+import java.lang.reflect.Field;
 import java.time.LocalDate;
+import java.util.Arrays;
 
 public class TestAnnotation {
     public static void main(String[] args) {
@@ -12,6 +14,31 @@ public class TestAnnotation {
         p.setDate(LocalDate.now());
         p.setPrice(5000L);
         p.setName("tv");
-        
+
+        Field[] att= p.getClass().getDeclaredFields();
+
+        String json= Arrays.stream(att)
+                .filter(f-> f.isAnnotationPresent(JsonAttribute.class))
+                .map(f->{
+                    f.setAccessible(true);
+                    String name= f.getAnnotation(JsonAttribute.class).name().equals("")
+                    ? f.getName() : f.getAnnotation(JsonAttribute.class).name();
+
+                    try {
+                        return "\""+name+"\":\""+f.get(p)+"\"";
+                    } catch (IllegalAccessException e) {
+                        throw new RuntimeException("error json: "+e.getMessage());
+                    }
+
+                } )
+                .reduce("{",(a,b)->{
+                    if("{".equals(a)){
+                        return a+b;
+                    }
+                    return a+", "+b;
+
+                }).concat("}");
+
+        System.out.println("json = " + json);
     }
 }
