ebe4a9ff86b21a0a446e4d3d4d0d5e4a62d1393d
vicente_ogalde <vicenteogalde@gmail.com>
2023-01-12 22:48:49 -0300

    section 32 commit n3

--- Section32JAVAMaster/src/cl/vicoga/desingpatterns/factory/PizzaShopNewYorkFactory.java
+++ Section32JAVAMaster/src/cl/vicoga/desingpatterns/factory/PizzaShopNewYorkFactory.java
@@ -1,9 +1,21 @@
 package cl.vicoga.desingpatterns.factory;
 
+import cl.vicoga.desingpatterns.factory.product.PizzaPepperoniNY;
+import cl.vicoga.desingpatterns.factory.product.PizzaVeggieNY;
+
 public class PizzaShopNewYorkFactory extends PizzaShopAbstractFactory{
 
     @Override
     ProductPizza makePizza(String type) {
-        return null;
+        ProductPizza pizza= null;
+        switch (type){
+            case"veggie":
+                pizza= new PizzaVeggieNY();
+                break;
+            case "pepperoni":
+                pizza= new PizzaPepperoniNY();
+        }
+
+        return pizza;
     }
 }
