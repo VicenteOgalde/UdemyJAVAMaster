8d7c4e65f4f3eba98b5e3b52a3fc0ab52881b28f
vicente_ogalde <vicenteogalde@gmail.com>
2023-01-16 12:30:57 -0300

    section 37 commit n0

---
+++ Section37JAVAMaster/src/main/java/cl/vicoga/junit5/test/model/BankAccount.java
@@ -0,0 +1,25 @@
+package cl.vicoga.junit5.test.model;
+
+import java.math.BigDecimal;
+
+public class BankAccount {
+
+    private String name;
+    private BigDecimal balance;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+}
