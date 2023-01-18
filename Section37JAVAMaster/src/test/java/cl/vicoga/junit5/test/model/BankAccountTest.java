12fff10e0edb297654dae88d732e9f67591a0cd3
vicente_ogalde <vicenteogalde@gmail.com>
2023-01-18 10:44:28 -0300

    section 37 commit n8

--- Section37JAVAMaster/src/test/java/cl/vicoga/junit5/test/model/BankAccountTest.java
+++ Section37JAVAMaster/src/test/java/cl/vicoga/junit5/test/model/BankAccountTest.java
@@ -10,22 +10,22 @@ import static org.junit.jupiter.api.Assertions.*;
 class BankAccountTest {
 
     @Test
-    void testAccountName(){
-        BankAccount account= new BankAccount("tex",new BigDecimal("1000.523"));
+    void testAccountName() {
+        BankAccount account = new BankAccount("tex", new BigDecimal("1000.523"));
 //        account.setName("tex");
-        String okName="tex";
-        String real=account.getName();
+        String okName = "tex";
+        String real = account.getName();
 
-        assertEquals(okName,real);
-
+        assertEquals(okName, real);
 
 
     }
+
     @Test
-    void testBalance(){
+    void testBalance() {
         BankAccount account = new BankAccount("tex", new BigDecimal("1000.123"));
-        assertEquals(1000.123,account.getBalance().doubleValue());
-        assertFalse(account.getBalance().compareTo(BigDecimal.ZERO)<0);
+        assertEquals(1000.123, account.getBalance().doubleValue());
+        assertFalse(account.getBalance().compareTo(BigDecimal.ZERO) < 0);
     }
 
     @Test
@@ -35,7 +35,7 @@ class BankAccountTest {
         BankAccount account2 = new BankAccount("john", new BigDecimal("1000.123"));
 
 //        assertNotEquals(account,account2);
-        assertEquals(account,account2);
+        assertEquals(account, account2);
 
 
     }
@@ -46,21 +46,22 @@ class BankAccountTest {
 
         account.debit(new BigDecimal(100));
 
-        assertEquals(900,account.getBalance().intValue());
+        assertEquals(900, account.getBalance().intValue());
 
-        assertEquals("900.123",account.getBalance().toPlainString());
+        assertEquals("900.123", account.getBalance().toPlainString());
 
 
     }
+
     @Test
     void testCreditAccount() {
         BankAccount account = new BankAccount("tex", new BigDecimal("1000.123"));
 
         account.credit(new BigDecimal(100));
 
-        assertEquals(1100,account.getBalance().intValue());
+        assertEquals(1100, account.getBalance().intValue());
 
-        assertEquals("1100.123",account.getBalance().toPlainString());
+        assertEquals("1100.123", account.getBalance().toPlainString());
 
 
     }
@@ -69,12 +70,12 @@ class BankAccountTest {
     void NoCreditException() {
         BankAccount account = new BankAccount("tex", new BigDecimal("1000.123"));
 
-        Exception e= assertThrows(NoCreditException.class,
-                ()->{
-            account.debit(new BigDecimal("1200.231"));
+        Exception e = assertThrows(NoCreditException.class,
+                () -> {
+                    account.debit(new BigDecimal("1200.231"));
 
                 });
-        assertEquals("not enough funds",e.getMessage());
+        assertEquals("not enough funds", e.getMessage());
     }
 
     @Test
@@ -83,12 +84,13 @@ class BankAccountTest {
         BankAccount account2 = new BankAccount("tex", new BigDecimal("5000.123"));
 
         Bank bank = new Bank();
-        bank.transfer(account,account2,new BigDecimal("800.123"));
+        bank.transfer(account, account2, new BigDecimal("800.123"));
 
-        assertEquals(account.getBalance().toPlainString(),"200.000");
-        assertEquals(account2.getBalance().toPlainString(),"5800.246");
+        assertEquals(account.getBalance().toPlainString(), "200.000");
+        assertEquals(account2.getBalance().toPlainString(), "5800.246");
 
     }
+
     @Test
     void testBankAccountsRelationship() {
         BankAccount account = new BankAccount("tex", new BigDecimal("1000.123"));
@@ -98,19 +100,30 @@ class BankAccountTest {
         bank.setName("b1");
         bank.addAccount(account);
         bank.addAccount(account2);
-        bank.transfer(account,account2,new BigDecimal("800.123"));
+        bank.transfer(account, account2, new BigDecimal("800.123"));
 
-        assertEquals(2,bank.getAccounts().size());
-        assertEquals("b1",account.getBank().getName());
+        assertAll(
+                () -> {
+                    assertEquals(2, bank.getAccounts().size());
+                }, () -> {
+                    assertEquals("tex", bank.getAccounts().stream()
+                            .filter(a -> a.getName().equals("tex")).findFirst().get().getName());
+                }, () -> {
+                    assertEquals("b1", account.getBank().getName());
+                }
+        );
 
-        assertEquals("tex",bank.getAccounts().stream()
-                .filter(a->a.getName().equals("tex")).findFirst().get().getName());
+        assertEquals(2, bank.getAccounts().size());
+        assertEquals("b1", account.getBank().getName());
+
+        assertEquals("tex", bank.getAccounts().stream()
+                .filter(a -> a.getName().equals("tex")).findFirst().get().getName());
 
         assertTrue(bank.getAccounts().stream()
-                .anyMatch(a->a.getName().equals("tex")));
+                .anyMatch(a -> a.getName().equals("tex")));
 
-        assertEquals(account.getBalance().toPlainString(),"200.000");
-        assertEquals(account2.getBalance().toPlainString(),"5800.246");
+        assertEquals(account.getBalance().toPlainString(), "200.000");
+        assertEquals(account2.getBalance().toPlainString(), "5800.246");
 
     }
 }
