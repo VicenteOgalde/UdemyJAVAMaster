0e7b56e29380be0f6bc02c7a9ad0aa2501591825
vicente_ogalde <vicenteogalde@gmail.com>
2023-01-28 09:01:45 -0300

    section 40 commit 1

--- Section39JAVAMaster/src/main/java/cl/vicoga/webapp/servlet/HelloWorldServlet.java
+++ Section39JAVAMaster/src/main/java/cl/vicoga/webapp/servlet/HelloWorldServlet.java
@@ -7,6 +7,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet("/hello-world")
 public class HelloWorldServlet extends HttpServlet {
@@ -14,6 +15,19 @@ public class HelloWorldServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request,
                          HttpServletResponse response)
     throws IOException, ServletException {
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.print("<!DOCTYPE html>");
+        out.print("<html>");
+        out.print("<head>");
+        out.print("<meta charset=\"UTF-8\">");
+        out.print("<title>Hello World</title>");
+        out.print("</head>");
+        out.print("<body>");
+        out.print("<h1>HELLO</h1>");
+        out.print("</body>");
+        out.print("</html>");
 
 
     }
