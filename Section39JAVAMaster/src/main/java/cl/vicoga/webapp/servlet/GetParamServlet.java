fc277372780f10bbe8461cd3b8c7935987d32a5d
vicente_ogalde <vicenteogalde@gmail.com>
2023-01-28 10:26:25 -0300

    section 40 commit 3

--- Section39JAVAMaster/src/main/java/cl/vicoga/webapp/servlet/GetParamServlet.java
+++ Section39JAVAMaster/src/main/java/cl/vicoga/webapp/servlet/GetParamServlet.java
@@ -18,6 +18,7 @@ public class GetParamServlet extends HttpServlet {
         PrintWriter out = resp.getWriter();
 
         String greet= req.getParameter("greet");
+        String name = req.getParameter("name");
 
         out.print("<!DOCTYPE html>");
         out.print("<html>");
@@ -27,7 +28,14 @@ public class GetParamServlet extends HttpServlet {
         out.print("</head>");
         out.print("<body>");
         out.print("<h1>Get Param</h1>");
-        out.print("<h2>"+greet+"</h2>");
+        if(greet!=null && name!=null){
+            out.print("<h2>"+greet+" "+name+"</h2>");
+        } else if (greet!=null) {
+            out.print("<h2>"+greet+"</h2>");
+        }else {
+            out.print("<h2>no greet for you</h2>");
+        }
+
         out.print("</body>");
         out.print("</html>");
     }
