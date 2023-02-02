957472bf5e77e05ad9e4dd6d000f3948165d6282
vicente_ogalde <vicenteogalde@gmail.com>
2023-02-02 11:03:32 -0300

    section 43 java master 1

--- Section43JAVAMaster/src/main/java/cl/vicoga/webappHeaders/controllers/LoginServlet.java
+++ Section43JAVAMaster/src/main/java/cl/vicoga/webappHeaders/controllers/LoginServlet.java
@@ -2,18 +2,47 @@ package cl.vicoga.webappHeaders.controllers;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.Optional;
 
-@WebServlet("/login")
+@WebServlet({"/login","/login.html"})
 public class LoginServlet extends HttpServlet {
 
     final static String USER="admin";
     final static String PASS="123";
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Cookie[]cookies= req.getCookies()!=null?req.getCookies():new Cookie[0];
+        Optional<Cookie>cookie= Arrays.stream(cookies).filter(c->c.getName().equals("user")).findFirst();
+
+        if(cookie.isPresent()){
+            resp.setContentType("text/html; charset=UTF-8");
+            try(PrintWriter out = resp.getWriter()) {
+                out.print("<!DOCTYPE html>");
+                out.print("<html>");
+                out.print("<head>");
+                out.print("<meta charset=\"UTF-8\">");
+                out.print("<title>Welcome "+cookie.get().getValue()+"</title>");
+                out.print("</head>");
+                out.print("<body>");
+                out.print("<h1>Welcome "+cookie.get().getValue()+"</h1>");
+                out.print("</body>");
+                out.print("</html>");
+            }
+        }else {
+            getServletContext().getRequestDispatcher("/login.jsp").forward(req,resp);
+        }
+
+    }
+
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
@@ -21,6 +50,8 @@ public class LoginServlet extends HttpServlet {
         String pass=req.getParameter("pass");
 
         if(USER.equals(user)&& PASS.equals(pass)){
+            Cookie userCookie= new Cookie("user",user);
+            resp.addCookie(userCookie);
 
             resp.setContentType("text/html; charset=UTF-8");
             try(PrintWriter out = resp.getWriter()) {
---
+++ Section43JAVAMaster/src/main/webapp/login.jsp
@@ -0,0 +1,32 @@
+<%@page contentType="text/html" pageEncoding="UTF-8" %>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+</head>
+<body>
+<h1>Login</h1>
+<form action="/Section43JAVAMaster/login" method="post">
+    <div>
+        <label for="user">Username</label>
+    <div>
+        <input type="text" name="user" id="user">
+    </div>
+    </div>
+    <div>
+        <label for="pass">Username</label>
+        <div>
+            <input type="text" name="pass" id="pass">
+        </div>
+    </div>
+    <div>
+        <input type="submit" value="Login">
+    </div>
+    
+
+</form>
+
+</body>
+</html>
