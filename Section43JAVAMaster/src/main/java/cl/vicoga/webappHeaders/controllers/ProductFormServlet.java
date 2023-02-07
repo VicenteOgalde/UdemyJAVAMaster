56c31e0118677dd2445d9b732e24cca18924acc0
vicente_ogalde <vicenteogalde@gmail.com>
2023-02-06 22:37:38 -0300

    section 48 java master 7

--- Section43JAVAMaster/src/main/java/cl/vicoga/webappHeaders/controllers/ProductFormServlet.java
+++ Section43JAVAMaster/src/main/java/cl/vicoga/webappHeaders/controllers/ProductFormServlet.java
@@ -1,5 +1,8 @@
 package cl.vicoga.webappHeaders.controllers;
 
+import cl.vicoga.webappHeaders.models.Product;
+import cl.vicoga.webappHeaders.service.ProductService;
+import cl.vicoga.webappHeaders.service.ProductServiceJDBCImpl;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -7,6 +10,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.sql.Connection;
 
 
 @WebServlet("/products/form")
@@ -22,6 +26,22 @@ public class ProductFormServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        Connection conn=(Connection) req.getAttribute("conn");
+        ProductService service= new ProductServiceJDBCImpl(conn);
+        int price;
+        try{
+            price=Integer.parseInt(req.getParameter("price"));
+        }catch (NumberFormatException e){
+            price=0;
+        }
+        service.save(
+                new Product(null,
+                        req.getParameter("name"),
+                        req.getParameter("type"),
+                        price)
+        );
+        resp.sendRedirect(req.getContextPath()+"/products");
 
     }
 }
