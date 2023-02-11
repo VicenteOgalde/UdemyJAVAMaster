955a10d248b6b99ec1ea592b956f5e549d057525
vicente_ogalde <vicenteogalde@gmail.com>
2023-02-11 16:29:31 -0300

    section 55 java master 7

---
+++ Section55JAVAMaster/src/main/java/Section55JAVAMaster/HibernateEdit.java
@@ -0,0 +1,36 @@
+package Section55JAVAMaster;
+
+import Section55JAVAMaster.entity.Client;
+import Section55JAVAMaster.util.JpaUtil;
+import jakarta.persistence.EntityManager;
+
+import javax.swing.*;
+
+public class HibernateEdit {
+
+    public static void main(String[] args) {
+
+        EntityManager em = JpaUtil.getEntityManager();
+        try {
+
+
+            Long id= Long.valueOf(JOptionPane.showInputDialog("Type id of the client to update"));
+
+            Client up=em.find(Client.class,id);
+            String name= JOptionPane.showInputDialog("type new client name: "+up.getName());
+            String surname= JOptionPane.showInputDialog("type new client surname: "+up.getSurname());
+            String payment= JOptionPane.showInputDialog("type new client payment method: "+up.getPaymentMethod());
+
+
+            em.getTransaction().begin();
+            Client c= new Client(up.getId(),name,surname,payment);
+            em.merge(c);
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            em.getTransaction().rollback();
+            e.printStackTrace();
+        } finally {
+            em.close();
+        }
+    }
+}
