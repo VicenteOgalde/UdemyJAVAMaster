7aef61a7326a4682c9813217deb5556d731469b6
vicente_ogalde <vicenteogalde@gmail.com>
2023-02-11 15:45:00 -0300

    section 55 java master 4

--- Section55JAVAMaster/src/main/java/Section55JAVAMaster/MainWhere.java
+++ Section55JAVAMaster/src/main/java/Section55JAVAMaster/MainWhere.java
@@ -3,14 +3,18 @@ package Section55JAVAMaster;
 import Section55JAVAMaster.entity.Client;
 import Section55JAVAMaster.util.JpaUtil;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.Query;
 
 import java.util.List;
 
-public class Main {
+public class MainWhere {
+
     public static void main(String[] args) {
         EntityManager em = JpaUtil.getEntityManager();
-        List<Client>clients=em.createQuery("select c from Client c",Client.class).getResultList();
-        clients.forEach(System.out::println);
+        Query query =em.createQuery("select c from Client c where c.paymentMethod=?1",Client.class);
+        query.setParameter(1,"cash");
+        Client c=(Client) query.getSingleResult();
+        System.out.println(c);
         em.close();
     }
 }
