f26249f3f980ed71e236ef871f0a918aa68b488a
vicente_ogalde <vicenteogalde@gmail.com>
2023-02-11 11:19:35 -0300

    section 55 java master 2

--- Section55JAVAMaster/src/main/java/Section55JAVAMaster/entity/Client.java
+++ Section55JAVAMaster/src/main/java/Section55JAVAMaster/entity/Client.java
@@ -1,4 +1,69 @@
 package Section55JAVAMaster.entity;
 
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="clients")
 public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private  String name;
+    private String surname;
+    @Column(name = "payment_method")
+    private String paymentMethod;
+
+    public Client() {
+    }
+
+    public Client(Long id, String name, String surname, String paymentMethod) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.paymentMethod = paymentMethod;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(String paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", paymentMethod='" + paymentMethod + '\'' +
+                '}';
+    }
 }
