c947e3b3f3a35ee82bfd1621727683d5b55df5bb
vicente_ogalde <vicenteogalde@gmail.com>
2023-02-12 20:12:12 -0300

    section 57 java master 0

---
+++ Section55JAVAMaster/src/main/java/Section55JAVAMaster/HibernateCriteria.java
@@ -0,0 +1,39 @@
+package Section55JAVAMaster;
+
+import Section55JAVAMaster.entity.Client;
+import Section55JAVAMaster.util.JpaUtil;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.ParameterExpression;
+import jakarta.persistence.criteria.Root;
+
+import java.util.List;
+
+public class HibernateCriteria {
+    public static void main(String[] args) {
+
+        EntityManager em= JpaUtil.getEntityManager();
+
+        CriteriaBuilder criteria=em.getCriteriaBuilder();
+        CriteriaQuery<Client> query=criteria.createQuery(Client.class);
+        Root<Client> from=query.from(Client.class);
+        query.select(from);
+        List<Client>clients=em.createQuery(query).getResultList();
+
+        clients.forEach(c-> System.out.println(c.getName()));
+
+        System.out.println("=========list where equals========");
+
+        query=criteria.createQuery(Client.class);
+        from=query.from(Client.class);
+        ParameterExpression<String> nameP=criteria.parameter(String.class,"name");
+        query.select(from).where(criteria.equal(from.get("name"),nameP));
+        clients=em.createQuery(query).setParameter("name","user1").getResultList();
+        clients.forEach(System.out::println);
+
+
+        em.close();
+
+    }
+}
